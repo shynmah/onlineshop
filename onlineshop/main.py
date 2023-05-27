325f352587116f4ab444a73eafb784522e322b28
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2023-05-27 21:38:48 +0330

    #

--- onlineshop/main.py
+++ onlineshop/main.py
@@ -1,4 +1,6 @@
 import sqlite3
+from logo import logo
+
 
 def connection(path):
     connector = sqlite3.connect(path)
@@ -7,54 +9,58 @@ def connection(path):
 
 
 def login(connector, cursor):
-    username = input("username: ")
-    password = input("password: ")
-    statement = f"SELECT username FROM users WHERE username='{username}' AND password = '{password}';"
-    cursor.execute(statement)
-    if not cursor.fetchone():
-        print("**Username or password is wrong**")
-        command = input("type login to try again or type register if you are new: ")
-        if command == "register":
-            register(connector, cursor)
-        elif command == "login":
-            login(connector, cursor)
-        else:
-            print("**command is wrong**")
-            login(connector, cursor)
+    print("**type register if you are new**")
+    username = input("enter your username: ")
+    if username == "register":
+        register(connector, cursor)
     else:
-        print("Welcome...")
-        mainpage()
+        password = input("enter your password: ")
+        statement = f"SELECT username FROM users WHERE username='{username}' AND password = '{password}';"
+        cursor.execute(statement)
+        if not cursor.fetchone():
+            print("**Username or password is wrong**")
+            command = input("type login to try again or type register if you are new: ")
+            if command == "register":
+                register(connector, cursor)
+            elif command == "login":
+                login(connector, cursor)
+            else:
+                print("**command is wrong**")
+                login(connector, cursor)
+        else:
+            statement = f"SELECT user_id FROM users WHERE username='{username}' AND password = '{password}';"
+            cursor.execute(statement)
+            tuple_user_id = cursor.fetchone()
+            global str_user_id
+            str_user_id = tuple_user_id[0]
+            mainpage()
+
 
 
 def register(connector, cursor):
     new_username = input("enter a username: ")
-    new_password = input("enter a password: ")
-    confirm_password = input("confirm your password: ")
-    if new_password == confirm_password:
-        data_tuple = (new_username, new_password)
-        cursor.execute("INSERT INTO users VALUES(?, ?)", data_tuple)
-        connector.commit()
-        print("you registerd successfully")
-        login(connector, cursor)
+    statement = f"SELECT username FROM users WHERE username='{new_username}' ;"
+    cursor.execute(statement)
+    if not cursor.fetchone() and new_username != "register":
+        new_password = input("enter a password: ")
+        confirm_password = input("confirm your password: ")
+        if new_password == confirm_password:
+            data_tuple = (new_username, new_password)
+            cursor.execute("INSERT INTO users VALUES(?, ?)", data_tuple)
+            connector.commit()
+            print("you registerd successfully")
+            login(connector, cursor)
+        else:
+            print("**passwords are not matched**")
+            register(connector, cursor)
     else:
-        print("**passwords are not matched**")
+        print("**username is not unique**")
         register(connector, cursor)
 
 
+
 def mainpage():
-    print("""
-                        _..-'(                       )`-.._
-                   ./'. '||\\.       (\_/)       .//||` .`\.
-                ./'.|'.'||||\\|..    )O O(    ..|//||||`.`|.`\.
-             ./'..|'.|| |||||\`````` '`"'` ''''''/||||| ||.`|..`\.
-           ./'.||'.|||| ||||||||||||.     .|||||||||||| |||||.`||.`\.
-          /'|||'.|||||| ||||||||||||{     }|||||||||||| ||||||.`|||`\
-         '.|||'.||||||| ||||||||||||{     }|||||||||||| |||||||.`|||.`
-        '.||| ||||||||| |/'   ``\||``     ''||/''   `\| ||||||||| |||.`
-        |/' \./'     `\./         \!|\   /|!/         \./'     `\./ `\|
-        V    V         V          }' `\ /' `{          V         V    V
-        `    `         `               V               '         '    '
-        """)
+    logo()
     print("avalible commands: products - orders - profile")
     command = input("type your command: ")
     if command == "products":
@@ -62,7 +68,7 @@ def mainpage():
     elif command == "cart":
         pass
     elif command == "profile":
-        pass
+        profile(connector, cursor)
     else:
         print("**command is wrong**")
         mainpage()
@@ -80,7 +86,22 @@ def products(connector, cursor):
         mainpage()
 
 def profile(connector, cursor):
-    statement = f"SELECT username, password FROM users WHERE category='{category}' "
+    statement = f"SELECT username, password FROM users WHERE user_id='{str_user_id}' "
+    info = cursor.execute(statement)
+    print("your information:", list(info))
+    Q = input("type 1 you want to change your password: ")
+    if Q == "1":
+        new_password = input("enter your new password: ")
+        statement = f"UPDATE users SET password='{new_password}' WHERE user_id={str_user_id} "
+        cursor.execute(statement)
+        connector.commit()
+        print("your password changed successfully")
+    else:
+        print("**command is wrong**")
+        mainpage()
+ 
+
+
 
 if __name__ == "__main__":
     connector, cursor = connection('data.db')
